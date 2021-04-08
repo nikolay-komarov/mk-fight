8a18bfa0a66f8047ec3d573c1718c738d3e92f6f
nikolay-komarov <kny.komarov@gmail.com>
2021-04-08 20:45:29 +0300

    task #1: добавит функцию createPlayer

--- main.js
+++ main.js
@@ -23,3 +23,34 @@ const sonya = {
 scorpion.attack();
 sonya.attack();
 
+const createPlayer = function () {
+  const $player1 = document.createElement('div');
+  $player1.classList.add('player1');
+
+  const $progressbar = document.createElement('div');
+  $progressbar.classList.add('progressbar');
+
+  const $life = document.createElement('div');
+  $life.classList.add('live');
+
+  const $name = document.createElement('div');
+  $name.classList.add('name');
+  $name.innerText = 'scorpion';
+
+  const $character = document.createElement('div');
+  $character.classList.add('character');
+
+  const $image = document.createElement('img');
+  $image.src='http://reactmarathon-api.herokuapp.com/assets/scorpion.gif';
+
+  $player1.appendChild($progressbar);
+  $progressbar.appendChild($life);
+  $progressbar.appendChild($name);
+  $player1.appendChild($character);
+  $character.appendChild($image);
+
+  return $player1;
+};
+
+console.log(createPlayer());
+
