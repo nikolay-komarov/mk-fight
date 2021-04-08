0a234523a2f181e1644f18807b56f7ef433cc506
nikolay-komarov <kny.komarov@gmail.com>
2021-04-08 19:55:28 +0300

    task #0: добавит два объекта игроков

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
 console.log('Fight...');
+
+const scorpion = {
+  name: 'scorpion',
+  hp: 100,
+  img: 'img',
+  weapon: ['weapon1', 'weapon2'],
+  attack: function() {
+    console.log(scorpion.name + ' fight...');
+  },
+};
+
+const sonya = {
+  name: 'sonya',
+  hp: 100,
+  img: 'img',
+  weapon: ['weapon1', 'weapon2'],
+  attack: function() {
+    console.log(sonya.name + ' fight...');
+  },
+};
+
+scorpion.attack();
+sonya.attack();
+
